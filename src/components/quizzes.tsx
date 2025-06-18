cc9d508b45c1d8d67fbd2419bafb0eeea5de3026
Paulo Sergio <contato.paulofws@gmail.com>
2025-06-18 17:38:04 -0300

    feat(quizzes): add quizzes component

---
+++ src/components/quizzes.tsx
@@ -0,0 +1,37 @@
+import { motion } from "motion/react";
+import { cards } from "@/cards";
+import { Card } from "./card";
+
+const cardsVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+export function Quizzes() {
+  return (
+    <section id="quizzes">
+      <motion.ul
+        variants={cardsVariants}
+        initial="hidden"
+        animate="show"
+        className="cards"
+        aria-label="Lista de Cards"
+      >
+        {cards.map((card) => (
+          <Card
+            href={card.href}
+            title={card.title}
+            description={card.description}
+            type="quiz"
+            key={card.id}
+          />
+        ))}
+      </motion.ul>
+    </section>
+  );
+}
