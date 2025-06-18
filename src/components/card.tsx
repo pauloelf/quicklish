7a969ed7e7d5194b570493fbb08d4ee80f83b8a1
Paulo Sergio <contato.paulofws@gmail.com>
2025-06-18 17:36:58 -0300

    feat(card): add card component

---
+++ src/components/card.tsx
@@ -0,0 +1,32 @@
+import { motion } from "motion/react";
+import type { CardType } from "@/types/Cards";
+import { Link } from "react-router";
+
+const cardVariants = {
+  hidden: { opacity: 0, x: -20 },
+  show: { opacity: 1, x: 0 },
+};
+
+export function Card({ type, title, description, href }: CardType) {
+  return (
+    <motion.li
+      variants={cardVariants}
+      className="card group-card"
+      aria-label={`Card do ${title}`}
+    >
+      <div className="card-info">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description" aria-label={description}>
+          {description}
+        </p>
+      </div>
+      <Link
+        to={type === "reading" ? `${href}/reading` : `${href}/quiz`}
+        className="card-button group-card"
+      >
+        {type === "reading" && "Iniciar Leitura"}
+        {type === "quiz" && "Iniciar Exercício"}
+      </Link>
+    </motion.li>
+  );
+}
