dfb92f90bacc9e4680c3a5376c1aef15f0ba9849
Paulo Sergio <contato.paulofws@gmail.com>
2025-06-18 17:36:25 -0300

    chore(card): add card data

---
+++ src/cards.ts
@@ -0,0 +1,64 @@
+import type { CardType } from "@/types/Cards";
+
+export const cards: CardType[] = [
+  {
+    id: 1,
+    title: "Articles",
+    description: "Artigos (A / An / The)",
+    href: "/articles",
+  },
+  {
+    id: 2,
+    title: "Verbo To Be",
+    description: "Verbo (Ser / Estar)",
+    href: "/verb-to-be",
+  },
+  {
+    id: 3,
+    title: "Family Vocabulary",
+    description: "Vocabulário da Família",
+    href: "/family-vocabulary",
+  },
+  {
+    id: 4,
+    title: "Imperatives",
+    description: "Imperativos",
+    href: "/imperatives",
+  },
+  {
+    id: 5,
+    title: "Can / Can’t",
+    description: "Habilidade e Permissão",
+    href: "/can-cant",
+  },
+  {
+    id: 6,
+    title: "Prepositions of Place",
+    description: "Preposições de Lugar",
+    href: "/prepositions-of-place",
+  },
+  {
+    id: 7,
+    title: "Question Words",
+    description: "Palavras Interrogativas",
+    href: "/question-words",
+  },
+  {
+    id: 8,
+    title: "There is / There are",
+    description: "(Há / Existe / Existem)",
+    href: "/there-is-are",
+  },
+  {
+    id: 9,
+    title: "Possessive Adjectives",
+    description: "Adjetivos Possessivos",
+    href: "/possessive-adjectives",
+  },
+  {
+    id: 10,
+    title: "Present Simple",
+    description: "Presente Simples",
+    href: "/present-simple",
+  },
+];
