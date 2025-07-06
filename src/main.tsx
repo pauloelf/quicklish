508e98a44015f5b945dd2ea0dea421d600f888e0
Paulo Sergio <contato.paulofws@gmail.com>
2025-07-06 19:18:14 -0300

    feat(routes): add routes (not-found, quiz and reading)

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,18 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "@/App";
 import "@/App.css";
+import QuizPage from "./pages/Quiz";
+import ReadingPage from "./pages/Reading";
+import NotFoundPage from "./pages/NotFound";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<App />} />
+        <Route path="/:id/reading" element={<ReadingPage />} />
+        <Route path="/:id/quiz" element={<QuizPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
